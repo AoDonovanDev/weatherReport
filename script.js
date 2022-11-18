53848e513a1358c2f5b09e4026657dd414ee7813
ao donovan <a.o.donovan.music@gmail.com>
2022-11-18 00:20:45 -0500

    try again

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const weatherPromise = fetch('https://api.openweathermap.org/data/2.5/weather?zip=37604&APPID=92e8bde80d10c60895c666edb58dd53b&units=imperial')
+const weatherPromise = fetch('https://api.openweathermap.org/data/2.5/weather?zip=37604&units=imperial&APPID=92e8bde80d10c60895c666edb58dd53b')
   .then((response) => response.json())
   .catch(function(err) {
     console.log("somethin is fucked " + err)
